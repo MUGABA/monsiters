057a893c1eedd87c5c614181ba0efae46ce682b8
MUGABA <mugabamuha@gmail.com>
2019-09-24 21:03:37 +0300

    Updates

---
+++ precache-manifest.0438e53e36e480347fd15864d0dbdda9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcea6ac511915b44a15bc46499257550",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "436e569ff5581b0662e1",
+    "url": "/monsters/static/css/main.bbb8e22f.chunk.css"
+  },
+  {
+    "revision": "4745097c3e5d72109586",
+    "url": "/monsters/static/js/2.96a442bc.chunk.js"
+  },
+  {
+    "revision": "436e569ff5581b0662e1",
+    "url": "/monsters/static/js/main.73b02b03.chunk.js"
+  },
+  {
+    "revision": "b4dbd1a619f832f4793b",
+    "url": "/monsters/static/js/runtime-main.9fc37942.js"
+  }
+]);
